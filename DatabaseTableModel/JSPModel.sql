a56554bd7e50d701f9c17a6520082a8ca5703f8b
Leafee98 <leafee98@hotmail.com>
2019-06-14 00:10:22 +0800

    add README.md and sql file to create table .

---
+++ DatabaseTableModel/JSPModel.sql
@@ -0,0 +1,92 @@
+drop database if exists questionnaire;
+create database questionnaire;
+use questionnaire;
+
+create table q_user (
+	uid int primary key auto_increment,
+	username char(30) not null unique,
+	passwd char(32) not null,
+	nickname nchar(100) not null
+);
+
+create table q_paper (
+	paperid int primary key auto_increment,
+	uid int,
+	paper_name nchar(100) not null unique,
+	publish_time datetime not null default NOW(),
+	cutoff_time datetime not null default adddate(NOW(), 3),
+	
+	constraint reference_of_ownerid
+		foreign key(uid) references q_user(uid),
+	constraint unique_paper_name_per_user
+		unique (uid, paper_name)
+);
+
+create table q_question (
+	questionid int primary key auto_increment,
+	paperid int,
+	question_type char(10) not null,
+	question nvarchar(1000) not null,
+	
+	constraint refenence_of_paperid
+		foreign key (paperid) references q_paper(paperid),
+	constraint almost_unique_question_per_paper
+		unique (paperid, question_type, question)
+);
+
+create table q_selection (
+	selectionid int primary key auto_increment,
+	questionid int,
+	selection_describe text not null,
+	
+	constraint reference_of_questionid
+		foreign key (questionid) references q_question(questionid)
+);
+
+create table q_answer (
+	answerid int primary key auto_increment,
+	questionid int,
+	answer text not null,
+	respondent int,
+	
+	constraint references_of_questionid
+		foreign key (questionid) references q_question(questionid),
+	constraint references_of_respondent
+		foreign key (respondent) references q_user(uid)
+);
+
+create table q_allow_answer (
+	paperid int not null,
+	uid int not null,
+	
+	constraint reference_of_destination_paperid
+		foreign key (paperid) references q_paper(paperid),
+	constraint reference_of_user_who_allow
+		foreign key (uid) references q_user(uid),
+	constraint primarykey_q_allow_answer
+		primary key (paperid, uid)
+);
+
+
+
+delimiter //
+create procedure check_user_passwd(in un char(30), in p char(32))
+begin
+	set @pass = (select passwd from q_user where username = un);
+	if @pass = md5(p) then
+		select 1 as result;
+	else
+		select 0 as result;
+	end if;
+end
+//
+delimiter ;
+
+
+delimiter //
+create trigger encry_passwd before insert on q_user for each row
+begin
+	set new.passwd = md5(new.passwd);
+end
+//
+delimiter ;
---
+++ DatabaseTableModel/JSPModelData.sql
@@ -0,0 +1,28 @@
+delete from q_answer;
+delete from q_selection;
+delete from q_question;
+delete from q_paper;
+delete from q_user;
+
+insert into q_user (q_user.uid, q_user.username, q_user.passwd, q_user.nickname) values 
+	(null, 'leafee', 'feng', 'Leafee');
+set @uid = (select uid from q_user where username = 'leafee');
+
+insert into q_paper (paperid, uid, paper_name, publish_time, cutoff_time) values
+	(null, @uid, 'test_paper', default, default);
+set @paperid = (select paperid from q_paper where uid = @uid);
+
+insert into q_question (questionid, paperid, question_type, question) values
+	(null, @paperid, "radio", 'test_question, please choose the current one');
+set @questionid = (select questionid from q_question where paperid = @paperid);
+
+insert into q_selection (selectionid, questionid, selection_describe) values
+	(null, @questionid, 'the current one');
+insert into q_selection (selectionid, questionid, selection_describe) values
+	(null, @questionid, 'the wrong one');
+set @selectionid = (select selectionid from q_selection where selection_describe = 'the current one');
+
+insert into q_allow_answer (paperid, uid) values (@paperid, @uid);
+	
+insert into q_answer (answerid, questionid, answer, respondent) values
+	(null, @questionid, @selectionid, @uid);
